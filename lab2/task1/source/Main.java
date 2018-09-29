1beb1110e0ca31c24b6d010fe215371b4896427e
chumaknadya <chumaknadya19@gmail.com>
2018-09-30 02:33:06 +0300

    Task 1 lab 2

--- lab2/task1/source/Main.java
+++ lab2/task1/source/Main.java
@@ -1,2 +1,13 @@
+
 public class Main {
+
+    public static void main(String[] args) {
+        Arguments str = new Arguments(args);
+        System.out.println("Значения длин строк \n" + str.toString());
+        System.out.println("Cтроки в порядке возрастания их длин: " + str.sort(args));
+        Creator creator = new Creator("Чумак");
+        System.out.println("Разработчик: "+ creator.getSurname());
+        System.out.println("Дата получения задания: " + creator.getTask());
+        System.out.println("Дата сдачи задания: " + creator.doneTask());
+    }
 }
